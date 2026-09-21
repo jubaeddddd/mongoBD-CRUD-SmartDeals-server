d04753db1c6c5930aeb9ffbaaa0150bfa545f215
jubaeddddd <jubaed.techie@gmail.com>
2026-09-21 17:08:55 +0600

    post products

---
+++ index.js
@@ -0,0 +1,43 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT||3000
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = "mongodb+srv://smartDealsUser:duzI3Ni26UIAxM4w@cluster0.durs5dg.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const db=client.db('smartDealsDB')
+    const productsCollection=db.collection('products')
+
+    app.post('/products',async(req,res)=>{
+       const newProducts=req.body
+       const result=await productsCollection.insertOne(newProducts)
+       console.log(result)
+       res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`port is running on ${port}`)
+})
